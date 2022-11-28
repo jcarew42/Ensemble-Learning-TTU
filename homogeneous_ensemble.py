df60f63b007b4b1c6d8ddd33e03c1e1c3d9c8ab1
Carew <jcarew42@tntech.edu>
2022-11-28 10:45:01 -0600

    Working on encoding

--- homogeneous_ensemble.py
+++ homogeneous_ensemble.py
@@ -47,23 +47,25 @@ def homog_ens(data, algorithm, number_of_preds):
     
     #  Classifiers available to the ensemble
     predictors = [DecisionTreeClassifier(max_depth=5),
-                  SVR(kernel="rbf", C=10000, tol=1e-5),
+                  SVR(kernel="rbf"),#SVR(kernel="rbf", C=10000, tol=1e-5),
                   KNeighborsClassifier(n_neighbors=random.randint(1,25))]    
 
     weak_preds = []
     weights = []
 
     for i in range(0,number_of_preds):
+        
         #  Sampling of Data
         train_set, test_set = split_train_test(data, 0.2)
-        X_vars = train_set.iloc[:,1:-1]
+        X_vars = train_set.iloc[:,:-1]
         X_labels = train_set.iloc[:,-1]
         Y = X_labels.to_numpy()
         X = X_vars.to_numpy()
+        
         #  Model training
         weak_lin_reg = predictors[algorithm]
         weak_lin_reg.fit(X, Y)
         weak_preds.append(weak_lin_reg)
-        weights.append(1 / mean_squared_error(weak_lin_reg.predict(test_set.iloc[:,1:-1].to_numpy()), 
+        weights.append(1 / mean_squared_error(weak_lin_reg.predict(test_set.iloc[:,:-1].to_numpy()), 
                                               test_set.iloc[:,-1].to_numpy()))
     return weights, weak_preds
